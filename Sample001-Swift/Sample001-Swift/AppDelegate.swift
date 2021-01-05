fd6be27e181f5f1b6b7959f129abcca055096f26
temosan <temosan@icloud.com>
2021-01-05 12:29:07 +0900

    Created Sample001, Build Target: iOS12

---
+++ Sample001-Swift/Sample001-Swift/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Sample001-Swift
+//
+//  Created by mjgu on 2021/01/05.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ Sample001-Swift/Sample001-Swift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Sample001-Swift
+//
+//  Created by mjgu on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { .default }
+}
+
